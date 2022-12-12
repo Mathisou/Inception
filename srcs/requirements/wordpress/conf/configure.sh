c089c3f156ec96714cf3211b3ed1e5962c6bac79
root <root@maroly.42.fr>
2022-12-12 17:52:52 +0100

    everything works well, dash connect on port 80 at first -> should be 443

--- srcs/requirements/wordpress/conf/configure.sh
+++ srcs/requirements/wordpress/conf/configure.sh
@@ -8,8 +8,7 @@ done
 if [ ! -f "/var/www/html/index.html" ]; then
 
 	wp core download --allow-root
-	#wp config create --dbname=$WP_DATABASE_NAME --dbuser=$WP_DATABASE_USER --dbpass=$WP_DATABASE_PASSWORD --dbhost=$MARIADB_ROOT_HOST --dbcharset=$MARIADB_CHARSET --dbcollate=$MARIADB_COLLATION --allow-root
-	mv ./wp-config.php /var/www/html/
+	wp config create --dbname=$WP_DATABASE_NAME --dbuser=$WP_DATABASE_USER --dbpass=$WP_DATABASE_PASSWORD --dbhost=$MARIADB_ROOT_HOST --dbcharset=$MARIADB_CHARSET --dbcollate=$MARIADB_COLLATION --allow-root
 	wp core install --url=$DOMAIN_NAME --title=$WP_TITLE --admin_user=$WP_ADMIN_USERNAME --admin_password=$WP_ADMIN_PASSWORD --admin_email=$WP_ADMIN_EMAIL --skip-email --allow-root
 	wp user create $WP_USERNAME $WP_EMAIL --role=author --user_pass=$WP_PASSWORD --allow-root
 
